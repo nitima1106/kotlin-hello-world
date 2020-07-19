e14224a980a8e32365cf6dfd14a7b3837bd33139
nitima1106 <68175887+nitima1106@users.noreply.github.com>
2020-07-19 12:26:18 +0700

    เขียนโปรแกรม yabbadabbado

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,9 +1,10 @@
 fun main(){
     var x = 1
-    println("Before the loop. x =$x.")
-    while (x < 4) {
-        println(" In the loop. x $x.")
+
+    while (x < 3) {
+        print (if (x == 1)"Yab" else "Dab")
+        print ("ba")
         x = x + 1
     }
-    println("After the loop. x = $x.")
+    if (x==3) println ("Do")
 }
